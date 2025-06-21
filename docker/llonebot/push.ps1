db71b147377f5d5b42dbb92a1c6b547114a67561
linyuchen <lin.yu.chen@hotmail.com>
2025-06-21 17:23:03 +0800

    Update Dockerfiles and build scripts for LLOneBot version 5.1.0 and PMHQ version 2.0.1

--- docker/llonebot/push.ps1
+++ docker/llonebot/push.ps1
@@ -1,2 +1,2 @@
-$version = "5.1.0"
+$version = "5.2.0"
 docker buildx build --platform linux/amd64,linux/arm64 -t "linyuchen/llonebot:$version" -t "linyuchen/llonebot:latest" -f docker/llonebot/Dockerfile --push .
