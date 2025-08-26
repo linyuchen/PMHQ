124fd9a03db31d65fc7315474098ab4b9cd1ea7c
linyuchen <lin.yu.chen@hotmail.com>
2025-08-26 17:02:23 +0800

    refactor: remove llob docker

--- docker/llonebot/build-amd64.sh
+++
@@ -1,2 +0,0 @@
-version=5.3.0
-docker build --build-arg LLONEBOT_VERSION=$version --progress=plain --platform linux/amd64 -t linyuchen/llonebot:$version -t linyuchen/llonebot:latest -f docker/llonebot/Dockerfile .
--- docker/llonebot/build-arm64.sh
+++
@@ -1,2 +0,0 @@
-version=5.3.0
-docker build --build-arg LLONEBOT_VERSION=$version --platform linux/arm64 -t linyuchen/llonebot:$version -t linyuchen/llonebot:latest -f docker/llonebot/Dockerfile .
