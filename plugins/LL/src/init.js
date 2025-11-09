652a1567cefb6150147ce319602ea8e61bbffc2b
linyuchen <lin.yu.chen@hotmail.com>
2025-11-09 10:32:12 +0800

    Add submodule for LiteLoaderQQNT plugins

--- plugins/LL/src/init.js
+++
@@ -1,9 +0,0 @@
-require("./ll_api/main.js");
-require("./loader_core/plugin_loader.js");
-require("./main.js");
-
-if (!globalThis.qwqnt) {
-    const main_path = "./application.asar/app_launcher/index.js";
-    require(require("path").join(process.resourcesPath, "app", main_path));
-    setImmediate(() => global.launcher.installPathPkgJson.main = main_path);
-}
